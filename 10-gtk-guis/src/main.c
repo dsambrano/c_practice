b1df2cb7f8c77faccbde14cda6162a72c29888c3
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-12-31 19:51:05 -0800

    New Feature: Added Button and Button Counter

--- 10-gtk-guis/src/main.c
+++ 10-gtk-guis/src/main.c
@@ -1,21 +1,37 @@
 #include <gtk/gtk.h>
 
+int count = 0;
+
 void end_program(GtkWidget *wid, gpointer ptr){
     gtk_main_quit();
 }
 
+void button_counter(GtkWidget *wid, gpointer ptr){
+    char updated_label_with_count[30];
+    count ++;
+    sprintf(updated_label_with_count, "Button Pressed %d Times", count);
+    gtk_label_set_text(GTK_LABEL(ptr), updated_label_with_count);
+}
+
 int main (int argc, char *argv[]){
+
+    // Initialize GTK
     gtk_init(&argc, &argv);
+    
     // Creating a GTK window
     GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    
     // Creating GTK butten widget with label
     GtkWidget *btn = gtk_button_new_with_label("Close Window");
-    // add callback function to button clicked and close app events
-    g_signal_connect(btn, "clicked", G_CALLBACK(end_program), NULL);
-    g_signal_connect(win, "delete_event", G_CALLBACK(end_program), NULL);
+    GtkWidget *btn2 = gtk_button_new_with_label("Add Count");
 
     // Adding a label
     GtkWidget *lbl = gtk_label_new("My Label");
+    
+    // add callback function to button clicked and close app events
+    g_signal_connect(btn, "clicked", G_CALLBACK(end_program), NULL);
+    g_signal_connect(win, "delete_event", G_CALLBACK(end_program), NULL);
+    g_signal_connect(btn2, "clicked", G_CALLBACK(button_counter), lbl);
 
     // Adding Box to store all widgets
     // Creates a Vertical box with 5 pixels of spacing
