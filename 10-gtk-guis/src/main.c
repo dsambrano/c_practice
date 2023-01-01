728ffe2f398f0aaec405f6598c30969f51a1684e
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-12-31 19:10:18 -0800

    Adding Comments to help remember

--- 10-gtk-guis/src/main.c
+++ 10-gtk-guis/src/main.c
@@ -6,12 +6,18 @@ void end_program(GtkWidget *wid, gpointer ptr){
 
 int main (int argc, char *argv[]){
     gtk_init(&argc, &argv);
+    // Creating a GTK window
     GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    // Creating GTK butten widget with label
     GtkWidget *btn = gtk_button_new_with_label("Close Window");
+    // add callback function to button clicked and close app events
     g_signal_connect(btn, "clicked", G_CALLBACK(end_program), NULL);
     g_signal_connect(win, "delete_event", G_CALLBACK(end_program), NULL);
+    // Store the button in the window to be displayed
     gtk_container_add(GTK_CONTAINER(win), btn);
+    // Show the window widget
     gtk_widget_show_all(win);
+    // Run the main loop
     gtk_main();
     return 0;
 }
